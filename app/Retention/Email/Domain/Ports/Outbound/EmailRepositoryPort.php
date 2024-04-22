4c954bebd2b07f12d084a90dafeb57c50bc3c09a
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-22 15:57:42 +0200

    created emails for user events

--- app/Retention/Email/Domain/Ports/Outbound/EmailRepositoryPort.php
+++ app/Retention/Email/Domain/Ports/Outbound/EmailRepositoryPort.php
@@ -7,4 +7,8 @@ use App\Retention\Email\Domain\Model\Email;
 interface EmailRepositoryPort
 {
     public function getWelcomeEmail(string $to, string $name): Email;
+    public function getUpdateEmail(string $to, string $name): Email;
+    public function getBlockedEmail(string $to, string $name): Email;
+    public function getUnblockedEmail(string $to, string $name): Email;
+    public function getDeletedUserEmail(string $to, string $name): Email;
 }
--- app/Shared/Infraestructure/Listeners/UserCreatedListener.php
+++ app/Shared/Infraestructure/Listeners/UserCreatedListener.php
@@ -3,11 +3,8 @@
 namespace App\Shared\Infraestructure\Listeners;
 
 use App\Retention\Email\Application\SendEmailCommand;
-use App\Retention\Email\Application\SendWelcomeEmail\SendWelcomeEmailCommand;
 use App\Shared\Domain\Bus\Command\CommandBus;
-use App\Shared\Domain\Bus\Events\EventBus;
 use App\UserManagement\Domain\Events\UserCreatedEvent;
-use App\UserManagement\Domain\Events\UserDeletedEvent;
 
 class UserCreatedListener
 {
