8938bb6dfd6d3b61ef45d902bcc644e5ad6a0100
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-23 09:46:40 +0200

    Eloquent implementation of repository

--- app/Review/Infraestructure/Repositories/Models/EloquentReviewModel.php
+++ app/Review/Infraestructure/Repositories/Models/EloquentReviewModel.php
@@ -2,7 +2,23 @@
 
 namespace App\Review\Infraestructure\Repositories\Models;
 
-class EloquentReviewModel
-{
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
+class EloquentReviewModel extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'reviews';
+
+    protected $primaryKey = 'uuid';
+
+    protected $fillable = [
+        'uuid',
+        'reviewer_id',
+        'reviewer_id',
+        'review',
+        'rating'
+    ];
 }
---
+++ app/Review/Infraestructure/Repositories/ReviewEloquentRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Review\Infraestructure\Repositories;
+
+use App\Review\Domain\Ports\Outbound\ReviewRepositoryPort;
+use App\Review\Infraestructure\Repositories\Models\EloquentReviewModel;
+
+class ReviewEloquentRepository implements ReviewRepositoryPort
+{
+    public function findByUserUuid(string $userUuid)
+    {
+        return EloquentReviewModel::query()->where("user_id", $userUuid)->get();
+    }
+
+    public function create(array $data)
+    {
+        return EloquentReviewModel::query()->create($data);
+    }
+
+    public function updateRating($uuid, int $rating)
+    {
+        return EloquentReviewModel::query()->where('uuid', $uuid)->update(['rating' => $rating]);
+    }
+
+    public function updateDescription(string $uuid, string $description)
+    {
+        return EloquentReviewModel::query()->where('uuid', $uuid)->update(['description' => $description]);
+    }
+}
