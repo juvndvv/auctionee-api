41b22ab7f95811fe1ec451bf7d9716091bfc64a4
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-23 20:24:05 +0200

    migration for table wallets

---
+++ database/migrations/2024_04_23_181749_create_wallets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->string('uuid')->primary();
+            $table->float('amount');
+            $table->string('user_uuid');
+            $table->timestamps();
+
+            $table->foreign('user_uuid')->references('uuid')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
