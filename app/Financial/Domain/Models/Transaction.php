4fd93e4f88b6fd2d939456f0d1246560525702dd
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-23 22:27:47 +0200

    created transaction entity

--- app/Financial/Domain/Models/Transaction.php
+++ app/Financial/Domain/Models/Transaction.php
@@ -2,7 +2,43 @@
 
 namespace App\Financial\Domain\Models;
 
+use App\Financial\Domain\Models\ValueObjects\TransactionAmount;
+use App\Financial\Domain\Models\ValueObjects\WalletUuid;
+
 class Transaction
 {
+    private WalletUuid $destinationWalletUuid;
+    private TransactionAmount $amount;
 
+    public function __construct(string $destinationWalletUuid, float $amount)
+    {
+        $this->destinationWalletUuid = new WalletUuid($destinationWalletUuid);
+        $this->amount = new TransactionAmount($amount);
+    }
+
+    public static function fromPrimitives(array $data): self
+    {
+        return new self(
+            $data['destination_wallet_uuid'],
+            $data['amount']
+        );
+    }
+
+    public function toPrimitives(): array
+    {
+        return [
+            $this->destinationWalletUuid->value(),
+            $this->amount->value(),
+        ];
+    }
+
+    public function destinationWalletUuid(): string
+    {
+        return $this->destinationWalletUuid->value();
+    }
+
+    public function amount(): float
+    {
+        return $this->amount->value();
+    }
 }
