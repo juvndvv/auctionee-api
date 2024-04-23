5abd1bf859c4d006569705e9a08ac8a22c74b357
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-23 20:38:01 +0200

    find wallet by user uuid use case

---
+++ app/Financial/Application/FindWalletByUserUuid/FindWalletByUserUuidQuery.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Financial\Application\FindWalletByUserUuid;
+
+use App\Shared\Domain\Bus\Query\Query;
+
+class FindWalletByUserUuidQuery extends Query
+{
+
+}
---
+++ app/Financial/Application/FindWalletByUserUuid/FindWalletByUserUuidQueryHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Financial\Application\FindWalletByUserUuid;
+
+use App\Shared\Domain\Bus\Query\QueryHandler;
+
+class FindWalletByUserUuidQueryHandler extends QueryHandler
+{
+
+}
