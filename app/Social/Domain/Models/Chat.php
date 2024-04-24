d0564e4202562fd9c0cba133802134c0d58055e2
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-24 20:38:08 +0200

    Domain: created Chat Model

--- app/Social/Domain/Models/Chat.php
+++ app/Social/Domain/Models/Chat.php
@@ -2,7 +2,40 @@
 
 namespace App\Social\Domain\Models;
 
+use App\UserManagement\Domain\Models\ValueObjects\UserId;
+use Illuminate\Support\Collection;
+
 class Chat
 {
+    private UserId $left;
+    private UserId $right;
 
+    private Collection $messages;
+
+    /**
+     * @param string $leftUuid
+     * @param string $rightUuid
+     * @param array $messages
+     */
+    public function __construct(string $leftUuid, string $rightUuid, array $messages = [])
+    {
+        $this->left = new UserId($leftUuid);
+        $this->right = new UserId($rightUuid);
+        $this->messages = new Collection($messages);
+    }
+
+    public function left(): string
+    {
+        return $this->left();
+    }
+
+    public function right(): string
+    {
+        return $this->right();
+    }
+
+    public function messages(): array
+    {
+        return $this->messages;
+    }
 }
