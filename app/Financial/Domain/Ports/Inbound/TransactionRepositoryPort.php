7a579c0ddb44bb3cce50200d58e7c263e74c692d
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-24 09:32:57 +0200

    added return types and modified inputs

--- app/Financial/Domain/Ports/Inbound/TransactionRepositoryPort.php
+++ app/Financial/Domain/Ports/Inbound/TransactionRepositoryPort.php
@@ -3,9 +3,19 @@
 namespace App\Financial\Domain\Ports\Inbound;
 
 use App\Financial\Domain\Models\Transaction;
+use Illuminate\Support\Collection;
 
 interface TransactionRepositoryPort
 {
-    public function findByWalletUuid(string $walletUuid);
-    public function create(array $data);
+    /**
+     * @param string $walletUuid
+     * @return Collection<Transaction>
+     */
+    public function findByWalletUuid(string $walletUuid): Collection;
+
+    /**
+     * @param Transaction $data
+     * @return void
+     */
+    public function create(Transaction $data): void;
 }
