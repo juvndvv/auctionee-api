ff6bd267d22102203c6ca2f01f1e3a422e0ad468
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-30 23:45:18 +0200

    fix(controller): initializing BaseRepository
    Author: Juan Daniel Forner

--- app/Auction/Infrastructure/Repositories/EloquentAuctionRepository.php
+++ app/Auction/Infrastructure/Repositories/EloquentAuctionRepository.php
@@ -2,9 +2,17 @@
 
 namespace App\Auction\Infrastructure\Repositories;
 
+use App\Auction\Domain\Ports\Outbound\AuctionRepositoryPort;
+use App\Auction\Infrastructure\Repositories\Models\EloquentAuctionModel;
 use App\Shared\Infrastructure\Repositories\BaseRepository;
 
-final class EloquentAuctionRepository extends BaseRepository
+final class EloquentAuctionRepository extends BaseRepository implements AuctionRepositoryPort
 {
+    public const string ENTITY_NAME = 'auction';
 
+    public function __construct()
+    {
+        parent::setEntityName(self::ENTITY_NAME);
+        parent::setModel(EloquentAuctionModel::query()->getModel());
+    }
 }
