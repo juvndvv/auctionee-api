a84b89ae35974c40d613353a2867bb04048690ef
Juan Daniel Forner <jdanielforga@gmail.com>
2024-04-30 11:43:51 +0200

    feat: added routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Auction\Infrastructure\Controllers\CreateCategoryController;
 use App\Financial\Infrastructure\Controllers\DepositMoneyController;
 use App\Financial\Infrastructure\Controllers\FindTransactionsByWalletUuidController;
 use App\Financial\Infrastructure\Controllers\FindWalletByUserUuidController;
@@ -15,7 +16,6 @@ use App\Review\Infrastructure\Controllers\UpdateRatingController;
 use App\Social\Infrastructure\Controllers\CreateChatRoomController;
 use App\Social\Infrastructure\Controllers\DeleteChatMessageController;
 use App\Social\Infrastructure\Controllers\FindChatRoomsByUserUuidController;
-use App\Social\Infrastructure\Controllers\FindFriendListByUserUuidController;
 use App\Social\Infrastructure\Controllers\FindMessagesByChatRoomUuidController;
 use App\Social\Infrastructure\Controllers\SendMessageController;
 use App\User\Infrastructure\Controllers\AuthenticateController;
@@ -34,7 +34,7 @@ use App\User\Infrastructure\Controllers\UpdateUserUsernameController;
 use Illuminate\Support\Facades\Route;
 
 // Users
-Route::post("/auth", AuthenticateController::class);
+Route::post('/auth', AuthenticateController::class);
 Route::post('/users', CreateUserController::class);
 Route::get('/users', FindAllUserController::class);
 Route::get('/users/{uuid}', FindUserByUuidController::class);
@@ -48,15 +48,15 @@ Route::put('/users/{uuid}/password', UpdateUserPasswordController::class);
 Route::get('/users/{uuid}/block', BlockUserController::class);
 Route::get('/users/{uuid}/unblock', UnblockUserController::class);
 Route::get('/users/{uuid}/reviews', FindUserReviewsController::class);
-Route::get("/users/{uuid}/rating", FindUserAverageRatingController::class);
-Route::get("/users/{uuid}/wallet", FindWalletByUserUuidController::class);
-Route::get("/users/{uuid}/chats", FindChatRoomsByUserUuidController::class);
+Route::get('/users/{uuid}/rating', FindUserAverageRatingController::class);
+Route::get('/users/{uuid}/wallet', FindWalletByUserUuidController::class);
+Route::get('/users/{uuid}/chats', FindChatRoomsByUserUuidController::class);
 
 // Wallets
-Route::get("/wallets/{uuid}/transfer", MakeTransactionController::class);
-Route::get("/wallets/{uuid}/transactions", FindTransactionsByWalletUuidController::class);
-Route::post("/wallets/{uuid}/deposit", DepositMoneyController::class);
-Route::post("/wallets/{uuid}/withdraw", WithdrawMoneyController::class);
+Route::get('/wallets/{uuid}/transfer', MakeTransactionController::class);
+Route::get('/wallets/{uuid}/transactions', FindTransactionsByWalletUuidController::class);
+Route::post('/wallets/{uuid}/deposit', DepositMoneyController::class);
+Route::post('/wallets/{uuid}/withdraw', WithdrawMoneyController::class);
 
 // Reviews
 Route::post('/reviews', PlaceReviewController::class);
@@ -65,11 +65,14 @@ Route::put('/reviews/{uuid}/rating', UpdateRatingController::class);
 Route::put('/reviews/{uuid}/description', UpdateDescriptionController::class);
 
 // Events
-Route::get("/events", FindAllEventsController::class);
+Route::get('/events', FindAllEventsController::class);
 
 // Chat rooms
-Route::post("/chats", CreateChatRoomController::class);
+Route::post('/chats', CreateChatRoomController::class);
 Route::post('chats/{uuid}', SendMessageController::class)->middleware('auth:sanctum');
-Route::get("/chats/{uuid}/messages", FindMessagesByChatRoomUuidController::class);
+Route::get('/chats/{uuid}/messages', FindMessagesByChatRoomUuidController::class);
 Route::get('/users/{uuid}/chats', FindChatRoomsByUserUuidController::class);
-Route::delete("/chats/{chatUuid}/messages/{messageUuid}", DeleteChatMessageController::class);
+Route::delete('/chats/{chatUuid}/messages/{messageUuid}', DeleteChatMessageController::class);
+
+// Categories
+Route::post('/categories', CreateCategoryController::class);
