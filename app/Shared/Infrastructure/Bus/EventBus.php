90ee067581d045ec7d6bd0f0f463f4e026427d2c
Juan Daniel Forner <jdanielforga@gmail.com>
2024-05-03 12:44:32 +0200

    fix: not saving private events

--- app/Shared/Infrastructure/Bus/EventBus.php
+++ app/Shared/Infrastructure/Bus/EventBus.php
@@ -3,6 +3,8 @@
 namespace App\Shared\Infrastructure\Bus;
 
 use App\Retention\EventMonitoring\Domain\Ports\Outbound\EventRepositoryPort;
+use App\Social\Domain\Events\MessageDeletedEvent;
+use App\Social\Domain\Events\MessageSentEvent;
 
 final readonly class EventBus
 {
@@ -14,13 +16,17 @@ final readonly class EventBus
         foreach(func_get_args() as $event) {
             event($event);
 
-            // Persist the event
-            $this->eventRepository->create([
-                'uuid' => $event->eventId,
-                'type' => $event->eventName(),
-                'payload' => json_encode($event->payload),
-                'occurred_on' => date('Y-m-d H:i:s', strtotime($event->occurredOn))
-            ]);
+            // Persist the event if needed
+            $unsavedEvents = [MessageSentEvent::eventName(), MessageDeletedEvent::eventName()];
+            if (!in_array($event->eventName(), $unsavedEvents)) {
+                $this->eventRepository->create([
+                    'uuid' => $event->eventId,
+                    'type' => $event->eventName(),
+                    'payload' => json_encode($event->payload),
+                    'occurred_on' => date('Y-m-d H:i:s', strtotime($event->occurredOn))
+                ]);
+            }
+
         }
     }
 }
