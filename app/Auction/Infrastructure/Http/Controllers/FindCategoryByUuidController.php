cf7be0729db191538cce28cec301857d7592deeb
Juan Daniel Forner <jdanielforga@gmail.com>
2024-05-20 15:29:16 +0200

    feat: find auctions like use case

--- app/Auction/Infrastructure/Http/Controllers/FindCategoryByUuidController.php
+++ app/Auction/Infrastructure/Http/Controllers/FindCategoryByUuidController.php
@@ -2,18 +2,17 @@
 
 namespace App\Auction\Infrastructure\Http\Controllers;
 
-use App\Auction\Domain\Ports\Outbound\CategoryRepositoryPort;
+use App\Auction\Domain\Models\Category\Category;
+use App\Auction\Infrastructure\Repositories\Models\EloquentCategoryModel;
 use App\Shared\Infrastructure\Http\Controllers\Response;
 
 class FindCategoryByUuidController
 {
-    public function __construct(private readonly CategoryRepositoryPort $categoryRepository)
-    {
-    }
-
     public function __invoke(string $uuid)
     {
-        $category = $this->categoryRepository->findByUuid($uuid);
-        return Response::OK($category, 'Categoria encontrada');
+        $category = EloquentCategoryModel::query()
+            ->where('uuid', $uuid)
+            ->first();
+        return Response::OK(Category::fromPrimitives($category->toArray()), 'Categoria encontrada');
     }
 }
