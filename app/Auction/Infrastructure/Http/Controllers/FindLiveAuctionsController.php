c71aaf62a1ecc896f02de7ac32973e3db84fac19
Juan Daniel Forner <jdanielforga@gmail.com>
2024-05-20 09:08:04 +0200

    feat: find live auctions use case

--- app/Auction/Infrastructure/Http/Controllers/FindLiveAuctionsController.php
+++ app/Auction/Infrastructure/Http/Controllers/FindLiveAuctionsController.php
@@ -40,6 +40,26 @@ final class FindLiveAuctionsController
             AuctionAndUserProjection::fromPrimitives($auctionModel->toArray())
         );
 
-        return Response::OK($resources, 'Subastas en directo encontradas');
+        return Response::OK(DB::select('
+SELECT
+    auctions.uuid as uuid,
+    auctions.name as name,
+    auctions.description as description,
+    auctions.starting_price as price,
+    auctions.starting_date as date,
+    auctions.duration as duration,
+    auctions.avatar as avatar,
+    users.uuid as user_uuid,
+    users.username as user_username,
+    users.avatar as user_avatar,
+    categories.uuid as category_uuid,
+    categories.name as category_name,
+    categories.avatar as category_avatar
+FROM auctions
+    JOIN users ON users.uuid = auctions.user_uuid
+    JOIN categories ON categories.uuid = auctions.category_uuid
+WHERE
+    NOW() BETWEEN auctions.starting_date AND DATE_ADD(auctions.starting_date, INTERVAL auctions.duration SECOND)
+        '), 'Subastas en directo encontradas');
     }
 }
