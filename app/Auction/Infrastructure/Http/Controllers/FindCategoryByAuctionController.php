aa5cbd7cf88240866f6e34663f6c49e205496401
Juan Daniel Forner <jdanielforga@gmail.com>
2024-05-21 10:02:19 +0200

    feat: find auctions like use case

---
+++ app/Auction/Infrastructure/Http/Controllers/FindCategoryByAuctionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Auction\Infrastructure\Http\Controllers;
+
+use App\Auction\Infrastructure\Repositories\Models\EloquentCategoryModel;
+
+final class FindCategoryByAuctionController
+{
+    public function __invoke(string $uuid)
+    {
+        $category = EloquentCategoryModel::query()
+            ->select([
+                'categories.uuid as uuid',
+                'categories.name as name',
+                'categories.description as description',
+                'categories.avatar as avatar',
+            ])->join('auctions', 'auctions.category_uuid', '=', 'categories.uuid')
+            ->where('auctions.uuid', $uuid);
+
+        return $category;
+    }
+}
