9887d9dfee98436632f47e08422d04e088afc5ca
Juan Daniel Forner <jdanielforga@gmail.com>
2024-05-23 14:27:53 +0200

    feat:  favorites persistence

--- app/Auction/Infrastructure/Repositories/Models/EloquentFavoriteModel.php
+++ app/Auction/Infrastructure/Repositories/Models/EloquentFavoriteModel.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class EloquentFavoriteModel extends Model
 {
     protected $table = 'user_auctions_favorites';
-    protected $fillable = ['user_id', 'auction_id'];
+    protected $fillable = ['user_uuid', 'auction_uuid'];
     public $timestamps = null;
 }
