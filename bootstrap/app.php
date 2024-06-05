0513829d15470b7b982d5b8b364211111e58af20
Juan Daniel Forner <jdanielforga@gmail.com>
2024-06-05 12:14:48 +0200

    dd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withMiddleware(function (Middleware $middleware) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function() {
+            \Illuminate\Support\Facades\Log::info('Hola');
+        })->everySecond();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
